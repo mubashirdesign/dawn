98354a1479b56286bdb5ab46c96c9a8ada46cfc1
unknown <you@example.com>
2022-06-11 07:06:31 -0400

    update mobile

--- src/js/app.js
+++ src/js/app.js
@@ -7,3 +7,4 @@ import '../../scripts/sections/collection-filters-form.js';
 import '../../scripts/sections/blog-filters.js';
 import '../../scripts/sections/product-gallery.js';
 import '../../scripts/sections/mobile-filters.js';
+import '../../scripts/sections/mobile-menu.js';
